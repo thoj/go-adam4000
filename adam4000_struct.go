e5c2777b2c6ee21496d95c2d858b0986b7f9b006
Thomas Jager <mail@jager.no>
2012-11-21 23:46:17 +0100

    Stuck in 2009 apparently

--- adam4000_struct.go
+++ adam4000_struct.go
@@ -1,4 +1,4 @@
-// Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+// Copyright 2012 Thomas Jager <mail@jager.no> All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
